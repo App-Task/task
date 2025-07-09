c00bd7c31110d633c8f4d790cc6b8be685ce0852
Yosuf AlAwadi <yosuf.alawadi@student.manchester.ac.uk>
2025-07-09 18:53:16 +0300

    Admin Panel #1

--- task-backend/routes/adminRoutes.js
+++ task-backend/routes/adminRoutes.js
@@ -69,7 +69,7 @@ router.get("/clients", async (req, res) => {
       _id: c._id,
       name: c.name,
       email: c.email,
-      image: c.profilePic || "",
+      image: c.profilePic || "/images/placeholder.png",
       isBlocked: !!c.isBlocked,
       totalTasks: taskMap[c._id.toString()] || 0
     }));
