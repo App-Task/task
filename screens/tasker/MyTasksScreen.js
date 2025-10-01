03d2ff847e1a2a8a90ca3ed70aa887284a882800
Yosuf AlAwadi <yosuf.alawadi@student.manchester.ac.uk>
2025-10-01 18:37:48 +0100

    Updated CMS

--- screens/tasker/MyTasksScreen.js
+++ screens/tasker/MyTasksScreen.js
@@ -420,7 +420,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#F8F8F8",
-    paddingTop: 60,
+    paddingTop: 24,
     paddingHorizontal: 20,
   },
   segmentedControl: {
